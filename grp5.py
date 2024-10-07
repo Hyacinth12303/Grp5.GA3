8b548ca83675bdf64c24133681c7372f04d227c9
Hyacinth12303 <47294038+Hyacinth12303@users.noreply.github.com>
2024-10-07 17:47:48 +0800

    Update grp5.py

--- grp5.py
+++ grp5.py
@@ -19,7 +19,12 @@ Reyes, Joanna Hyacinth M.
 
 # imports
 import streamlit as st
+import pandas as pd
+import numpy as np
 
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import seaborn as sns
 
 # Set seaborn styles
 sns.set_theme(style="whitegrid", palette="pastel")
